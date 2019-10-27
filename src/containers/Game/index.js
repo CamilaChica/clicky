83fac734ae776e87b237f9ed68b32e96f4410f5a
CamilaChica <andrekchir@gmail.com>
2019-10-27 00:47:12 -0400

    images and components added

--- src/containers/Game/index.js
+++ src/containers/Game/index.js
@@ -3,10 +3,34 @@ import Card from '../../components/Card';
 
 class Game extends Component
 {
+
     cards=[
-        
+        {
+        id:"choclate",
+        image:"https://www.modernhoney.com/wp-content/uploads/2018/08/Homemade-Chocolate-Ice-Cream-1.jpg"
+        },
+        {
+        id:"Tiramisu",
+        image:"http://assets.wholefoodsmarket.com/recipes/3503/2048/1536/3503-5.jpg"
+        },
+        {
+        id:"Berries",
+        image:"https://static.abc.es/media/summum/2018/05/04/frutos-rojos-kTs--1190x800@abc.jpg"
+        },
+        {
+        id:"Cookies and cream",
+        image:"https://www.simplyrecipes.com/wp-content/uploads/2018/02/Vegan-Mint-Chocolate-Chip-Ice-Cream-6-HORIZONTAL-LEAD.jpg"
+        },
     ]
 
+
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            cards:this.cards
+        }
+    }
 
     cardClicked=(card)=>{
         alert(card+" card clicked!")
@@ -17,10 +41,7 @@ class Game extends Component
         return <div>
         <h1>This is the clicky game</h1>
         <div className="row">
-            <Card onClick={()=>this.cardClicked("First")} image="https://www.modernhoney.com/wp-content/uploads/2018/08/Homemade-Chocolate-Ice-Cream-1.jpg" />
-            <Card onClick={()=>this.cardClicked("Second")} image="http://assets.wholefoodsmarket.com/recipes/3503/2048/1536/3503-5.jpg" />
-            <Card onClick={()=>this.cardClicked("Third")} image="https://static.abc.es/media/summum/2018/05/04/frutos-rojos-kTs--1190x800@abc.jpg" />
-            <Card onClick={()=>this.cardClicked("Fourth")} image="https://www.simplyrecipes.com/wp-content/uploads/2018/02/Vegan-Mint-Chocolate-Chip-Ice-Cream-6-HORIZONTAL-LEAD.jpg" />
+        {this.state.cards.map((card,index)=><Card key={index} onClick={()=>this.cardClicked(card)} image={card.image} />)}
         </div>
         </div>
     }
